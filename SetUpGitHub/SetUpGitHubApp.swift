05b841789ae18b663eb53b238cdc020e911ab52c
Paige <makipaige11@gmail.com>
2023-06-26 09:21:36 -0500

    Initial Commit

---
+++ SetUpGitHub/SetUpGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetUpGitHubApp.swift
+//  SetUpGitHub
+//
+//  Created by Paige Maki on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SetUpGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
